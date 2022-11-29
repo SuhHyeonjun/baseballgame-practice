195f469280de65679374c8cc7af55029b35015d8
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-29 18:40:20 +0900

    feat : 랜덤한 3자리의 숫자를 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+    private static final int NUMBER_SIZE = 3;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 9;
+
+    public List<Integer> createRandomNumbers() {
+        Set<Integer> duplicateRemover = new HashSet<>(NUMBER_SIZE);
+        while (duplicateRemover.size() < NUMBER_SIZE) {
+            int randomNumbers = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+            duplicateRemover.add(randomNumbers);
+        }
+        List<Integer> computerNumbers = new ArrayList<>(duplicateRemover);
+        return computerNumbers;
+    }
+}
