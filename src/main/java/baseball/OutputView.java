9ff191c4dd0a5d4cb961d065fb6e2f81eea7acc9
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 08:23:43 +0900

    fix : 스트라이크 볼 출력 오류 수정

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -1,45 +1,21 @@
 package baseball;
 
-import java.util.List;
-import java.util.Objects;
-
 public class OutputView {
-    private static final int NUMBER_SIZE = 3;
     private static final int ZERO = 0;
-    private int strikeCount = 0;
-    private int ballCount = 0;
 
-    public void printResult(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        printStrike(computerNumbers, playerNumber);
-        printBall(computerNumbers, playerNumber);
+    public void printResult(int strikeCount, int ballCount) {
         if (strikeCount == ballCount && strikeCount > ZERO) {
             System.out.println(strikeCount + Message.STRIKE.getMessage());
         }
         if (strikeCount == ZERO && ballCount > ZERO) {
             System.out.println(ballCount + Message.BALL.getMessage());
         }
-        if (strikeCount > ZERO && ballCount > ZERO) {
+        if (strikeCount > ZERO && ballCount - strikeCount > ZERO) {
             System.out.print((ballCount - strikeCount) + Message.BALL.getMessage());
             System.out.println(strikeCount + Message.STRIKE.getMessage());
         }
         if (strikeCount == ZERO && ballCount == ZERO) {
             System.out.println(Message.NOTHING.getMessage());
-        }
-    }
-
-    private void printStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (Objects.equals(computerNumbers.get(i), playerNumber.get(i))) {
-                strikeCount++;
-            }
-        }
-    }
-
-    private void printBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (computerNumbers.contains(playerNumber.get(i))) {
-                ballCount++;
-            }
         }
     }
 }
