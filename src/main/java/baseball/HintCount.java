c36ed0dad70e132f47018acb74a6809eca825f85
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 08:24:38 +0900

    feat : 스트라이크와 볼 개수를 구하는 기능

---
+++ src/main/java/baseball/HintCount.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import java.util.List;
+import java.util.Objects;
+
+public class HintCount {
+    private static final int NUMBER_SIZE = 3;
+    public static int strikeCount = 0;
+    public static int ballCount = 0;
+
+    public static void getHintCount(List<Integer> computerNumbers, List<Integer> playerNumber) {
+        getStrike(computerNumbers, playerNumber);
+        getBall(computerNumbers, playerNumber);
+    }
+
+    private static void getStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
+        for (int i = 0; i < NUMBER_SIZE; i++) {
+            if (Objects.equals(computerNumbers.get(i), playerNumber.get(i))) {
+                strikeCount++;
+            }
+        }
+    }
+
+    private static void getBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
+        for (int i = 0; i < NUMBER_SIZE; i++) {
+            if (computerNumbers.contains(playerNumber.get(i))) {
+                ballCount++;
+            }
+        }
+    }
+
+    public static int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public static int getBallCount() {
+        return ballCount;
+    }
+}
