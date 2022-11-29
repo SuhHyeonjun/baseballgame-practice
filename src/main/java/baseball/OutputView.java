51e91bd0b44ed25bd59badbb4844a2593df181da
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-29 22:01:49 +0900

    feat : 힌트 결과 출력

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -5,34 +5,42 @@ import java.util.Objects;
 
 public class OutputView {
     private static final int NUMBER_SIZE = 3;
+    private int strikeCount = 0;
+    private int ballCount = 0;
 
     public void printResult(List<Integer> computerNumbers, List<Integer> playerNumber) {
         printStrike(computerNumbers, playerNumber);
         printBall(computerNumbers, playerNumber);
+        if (strikeCount == ballCount) {
+            System.out.println(strikeCount + Message.STRIKE.getMessage());
+        }
+        if (strikeCount == 0 && ballCount > 0) {
+            System.out.println(ballCount + Message.BALL.getMessage());
+        }
+        if (strikeCount > 0 && ballCount > 0) {
+            System.out.print((ballCount - strikeCount) + Message.BALL.getMessage());
+            System.out.println(strikeCount + Message.STRIKE.getMessage());
+        }
         printNothing(computerNumbers, playerNumber);
     }
 
-    public void printStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        int strikeCount = 0;
+    private void printStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
         for (int i = 0; i < NUMBER_SIZE; i++) {
             if (Objects.equals(computerNumbers.get(i), playerNumber.get(i))) {
                 strikeCount++;
             }
         }
-        System.out.println(strikeCount + Message.STRIKE.getMessage());
     }
 
-    public void printBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        int ballCount = 0;
+    private void printBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
         for (int i = 0; i < NUMBER_SIZE; i++) {
             if (computerNumbers.contains(playerNumber.get(i))) {
                 ballCount++;
             }
         }
-        System.out.println(ballCount + Message.BALL.getMessage());
     }
 
-    public void printNothing(List<Integer> computerNumbers, List<Integer> playerNumber) {
+    private void printNothing(List<Integer> computerNumbers, List<Integer> playerNumber) {
         computerNumbers.retainAll(playerNumber);
         if (computerNumbers.size() == 0) {
             System.out.println(Message.NOTHING.getMessage());
