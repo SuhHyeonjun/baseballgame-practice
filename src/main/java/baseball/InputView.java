5605ab0219d1f43bb3ccfaf1b58b280cc171d8bd
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-29 20:29:48 +0900

    refactor : List를 반환하도록 변경

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -2,11 +2,21 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
-    public static String inputNumber() {
+    public List<Integer> inputNumber() {
         System.out.print(Message.INPUT_NUMBER.getMessage());
-        String playerNumber = readLine();
+        String inputNumber = readLine();
+        String[] stringArr = inputNumber.split("");
+        int[] intArr = new int[stringArr.length];
+        for (int i = 0; i < stringArr.length; i++) {
+            intArr[i] = Integer.parseInt(stringArr[i]);
+        }
+        List<Integer> playerNumber = Arrays.stream(intArr).boxed().collect(Collectors.toList());
         return playerNumber;
     }
 }
