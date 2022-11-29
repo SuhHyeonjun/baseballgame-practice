bc484b3b6ba7ca1ef08de4970f10bd2d47113a6a
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-29 17:37:59 +0900

    update : 초기 설정으로 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,7 @@
 package baseball;
 
-import static baseball.Message.START_GAME;
-
 public class Application {
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        System.out.println(START_GAME);
-        do {
-            baseballGame.start();
-        } while (baseballGame.endGame());
+
     }
 }
