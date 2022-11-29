da2bd9e64005b79956090da7703191ee14474889
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-29 20:53:34 +0900

    fix : 낫싱 출력 오류 수정

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -6,6 +6,12 @@ import java.util.Objects;
 public class OutputView {
     private static final int NUMBER_SIZE = 3;
 
+    public void printResult(List<Integer> computerNumbers, List<Integer> playerNumber) {
+        printStrike(computerNumbers, playerNumber);
+        printBall(computerNumbers, playerNumber);
+        printNothing(computerNumbers, playerNumber);
+    }
+
     public void printStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
         int strikeCount = 0;
         for (int i = 0; i < NUMBER_SIZE; i++) {
@@ -27,10 +33,9 @@ public class OutputView {
     }
 
     public void printNothing(List<Integer> computerNumbers, List<Integer> playerNumber) {
-        for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (!(computerNumbers.contains(playerNumber.get(i)))) {
-                System.out.println(Message.NOTHING.getMessage());
-            }
+        computerNumbers.retainAll(playerNumber);
+        if (computerNumbers.size() == 0) {
+            System.out.println(Message.NOTHING.getMessage());
         }
     }
 }
