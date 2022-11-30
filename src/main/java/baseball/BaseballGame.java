91aebe57e339467618f4c2f98fa98945ea9252c9
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 09:14:35 +0900

    feat : 3스트라이크 시 게임종료 메서드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,30 +1,41 @@
 package baseball;
 
-import static baseball.HintCount.getBallCount;
-import static baseball.HintCount.getHintCount;
-import static baseball.HintCount.getStrikeCount;
-
 import java.util.List;
 
 public class BaseballGame {
-    private static int strikeCount;
-    private static int ballCount;
-    private final Computer computer = new Computer();
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
+    private static final Computer computer = new Computer();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final HintCount hintCount = new HintCount();
+    private int strikeCount;
+    private int ballCount;
+    private static boolean status = true;
 
     public void start() {
         System.out.println(Message.START_GAME.getMessage());
         List<Integer> computerNumbers = computer.createRandomNumbers();
-        System.out.println(computerNumbers);
-        List<Integer> playerNumber = inputView.inputNumber();
-        getHintCount(computerNumbers, playerNumber);
-        applyHintCount();
-        outputView.printResult(strikeCount, ballCount);
+        do {
+            System.out.println(computerNumbers);
+            List<Integer> playerNumber = inputView.inputNumber();
+            hintCount.getHintCount(computerNumbers, playerNumber);
+            applyHintCount();
+            outputView.printResult(strikeCount, ballCount);
+            endGame(strikeCount);
+            hintCount.initCount();
+        } while (status);
+        System.out.println(Message.SUCCESS_GAME.getMessage());
     }
 
-    public static void applyHintCount() {
-        strikeCount = getStrikeCount();
-        ballCount = getBallCount();
+    private static boolean endGame(int strikeCount) {
+        status = true;
+        if (strikeCount == 3) {
+            status = false;
+        }
+        return status;
+    }
+
+    private void applyHintCount() {
+        this.strikeCount = hintCount.getStrikeCount();
+        this.ballCount = hintCount.getBallCount();
     }
 }
