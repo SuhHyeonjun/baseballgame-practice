7a16ada2b8eff6850f66b8012815a70d7ce54c27
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 09:46:57 +0900

    feat : 1~9의 숫자 범위가 아닐 시 예외 발생

---
+++ src/main/java/baseball/inputNumberValidator.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import java.util.Arrays;
+
+public class inputNumberValidator {
+
+    public static void validateInputNumber(String input) {
+        validateInputDuplicate(input);
+        validateInputSize(input);
+        validateInputRange(input);
+    }
+
+    private static void validateInputDuplicate(String input) {
+        String[] duplicateCheck = input.split("");
+        long size = Arrays.stream(duplicateCheck).distinct().count();
+        if (duplicateCheck.length != size) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_DUPLICATE.getErrorMessage());
+        }
+    }
+
+    private static void validateInputRange(String input) {
+        String regex = "[1-9]+";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_RANGE.getErrorMessage());
+        }
+    }
+}
