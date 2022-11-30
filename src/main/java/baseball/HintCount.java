e801fec6499a3eb7a96978e092c990cd554805d1
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 09:12:37 +0900

    update : 인스턴스 메서드로 변경

--- src/main/java/baseball/HintCount.java
+++ src/main/java/baseball/HintCount.java
@@ -5,20 +5,28 @@ import java.util.Objects;
 
 public class HintCount {
     private static final int NUMBER_SIZE = 3;
-    private static int strikeCount = 0;
-    private static int ballCount = 0;
+    private int strikeCount = 0;
+    private int ballCount = 0;
 
-    public static void getHintCount(List<Integer> computerNumbers, List<Integer> playerNumber) {
+    public void getHintCount(List<Integer> computerNumbers, List<Integer> playerNumber) {
         getStrike(computerNumbers, playerNumber);
         getBall(computerNumbers, playerNumber);
     }
 
-    public static void initCount() {
+    public void initCount() {
         strikeCount = 0;
         ballCount = 0;
     }
 
-    private static void getStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    private void getStrike(List<Integer> computerNumbers, List<Integer> playerNumber) {
         for (int i = 0; i < NUMBER_SIZE; i++) {
             if (Objects.equals(computerNumbers.get(i), playerNumber.get(i))) {
                 strikeCount++;
@@ -26,19 +34,11 @@ public class HintCount {
         }
     }
 
-    private static void getBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
+    private void getBall(List<Integer> computerNumbers, List<Integer> playerNumber) {
         for (int i = 0; i < NUMBER_SIZE; i++) {
             if (computerNumbers.contains(playerNumber.get(i))) {
                 ballCount++;
             }
         }
-    }
-
-    public static int getStrikeCount() {
-        return strikeCount;
-    }
-
-    public static int getBallCount() {
-        return ballCount;
     }
 }
