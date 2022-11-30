5b706034870ef22057d7b0ac4ee11ea5af24d884
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-30 09:29:14 +0900

    feat : 게임 종료 메시지 추가

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -2,14 +2,15 @@ package baseball;
 
 public enum Message {
     START_GAME("숫자 야구 게임을 시작합니다."),
-    INPUT_NUMBER("숫자를 입력해주세요."),
+    INPUT_NUMBER("숫자를 입력해주세요 : "),
     SUCCESS_GAME("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     ASK_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    ENDGAME("게임을 종료 합니다."),
     STRIKE("스트라이크"),
-    BALL("볼"),
+    BALL("볼 "),
     NOTHING("낫싱");
 
-    private String message;
+    private final String message;
 
     Message(String message) {
         this.message = message;
